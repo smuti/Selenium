0a0e0d85242655507933fde968a52acc84d4cb39
Sabri MUTLUCAG <sabri.mutlucag@t2.com.tr>
2016-04-09 14:40:59 +0300

    Initial commit

---
+++ RunSeleniumExample.py
@@ -0,0 +1,51 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+def runDriver(driver):
+    driver.get("http://www.python.org")
+    assert "Python" in driver.title
+    elem = driver.find_element_by_name("q")
+    elem.send_keys("pycon")
+    elem.send_keys(Keys.RETURN)
+    assert "No results found." not in driver.page_source
+    driver.close()
+
+driver = webdriver.Remote(
+   command_executor='http://127.0.0.1:4444/wd/hub',
+   desired_capabilities=DesiredCapabilities.CHROME)
+
+runDriver(driver)
+
+driver = webdriver.Remote(
+   command_executor='http://127.0.0.1:4444/wd/hub',
+   desired_capabilities=DesiredCapabilities.FIREFOX)
+
+runDriver(driver)
+
+
+
+# driver = webdriver.Remote(
+#    command_executor='http://127.0.0.1:4444/wd/hub',
+#    desired_capabilities=DesiredCapabilities.OPERA)
+#
+# driver.get("http://www.python.org")
+# assert "Python" in driver.title
+# elem = driver.find_element_by_name("q")
+# elem.send_keys("pycon")
+# elem.send_keys(Keys.RETURN)
+# assert "No results found." not in driver.page_source
+# driver.close()
+#
+# driver = webdriver.Remote(
+#    command_executor='http://127.0.0.1:4444/wd/hub',
+#    desired_capabilities=DesiredCapabilities.HTMLUNITWITHJS)
+#
+# driver.get("http://www.python.org")
+# assert "Python" in driver.title
+# elem = driver.find_element_by_name("q")
+# elem.send_keys("pycon")
+# elem.send_keys(Keys.RETURN)
+# assert "No results found." not in driver.page_source
+# driver.close()
