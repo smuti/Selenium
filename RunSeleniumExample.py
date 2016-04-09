627c4c46f8014d7d61e2b88b05af587a77798789
Sabri MUTLUCAG <sabri.mutlucag@t2.com.tr>
2016-04-09 14:41:28 +0300

    selenium example

--- RunSeleniumExample.py
+++ RunSeleniumExample.py
@@ -23,29 +23,3 @@ driver = webdriver.Remote(
    desired_capabilities=DesiredCapabilities.FIREFOX)
 
 runDriver(driver)
-
-
-
-# driver = webdriver.Remote(
-#    command_executor='http://127.0.0.1:4444/wd/hub',
-#    desired_capabilities=DesiredCapabilities.OPERA)
-#
-# driver.get("http://www.python.org")
-# assert "Python" in driver.title
-# elem = driver.find_element_by_name("q")
-# elem.send_keys("pycon")
-# elem.send_keys(Keys.RETURN)
-# assert "No results found." not in driver.page_source
-# driver.close()
-#
-# driver = webdriver.Remote(
-#    command_executor='http://127.0.0.1:4444/wd/hub',
-#    desired_capabilities=DesiredCapabilities.HTMLUNITWITHJS)
-#
-# driver.get("http://www.python.org")
-# assert "Python" in driver.title
-# elem = driver.find_element_by_name("q")
-# elem.send_keys("pycon")
-# elem.send_keys(Keys.RETURN)
-# assert "No results found." not in driver.page_source
-# driver.close()
